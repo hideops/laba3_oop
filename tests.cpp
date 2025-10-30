6ccb0d10831ca9ab13032f1e29fda551c50752cc
hideops <128249753+hideops@users.noreply.github.com>
2025-10-30 04:41:28 -0700

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -3,232 +3,233 @@
 #include <cmath>
 #include "main.cpp"
 
-TEST(TchkTest, Basic) {
-    Tchk t1;
-    EXPECT_DOUBLE_EQ(t1.dx, 0);
-    EXPECT_DOUBLE_EQ(t1.dy, 0);
+TEST(TchTest, Init) {
+    Tch t;
+    ASSERT_DOUBLE_EQ(t.x_, 0.0);
+    ASSERT_DOUBLE_EQ(t.y_, 0.0);
     
-    Tchk t2(2.5, 3.7);
-    EXPECT_DOUBLE_EQ(t2.dx, 2.5);
-    EXPECT_DOUBLE_EQ(t2.dy, 3.7);
+    Tch t2(1.5, -2.5);
+    ASSERT_DOUBLE_EQ(t2.x_, 1.5);
+    ASSERT_DOUBLE_EQ(t2.y_, -2.5);
 }
 
-TEST(TchkTest, Comparison) {
-    Tchk t1(1.1, 2.2), t2(1.1, 2.2), t3(1.1, 2.3);
-    EXPECT_TRUE(t1.eq(t2));
-    EXPECT_FALSE(t1.eq(t3));
+TEST(TchTest, Equal) {
+    Tch t1(1.0, 2.0);
+    Tch t2(1.0, 2.0);
+    Tch t3(1.0, 2.1);
+    
+    EXPECT_TRUE(t1 == t2);
+    EXPECT_FALSE(t1 == t3);
 }
 
-TEST(RmbTest, SquareCalculation) {
-    Tchk a(0,0), b(3,4), c(6,0), d(3,-4);
-    Rmb r(a,b,c,d);
-    
-    double expected = 24.0;
-    EXPECT_NEAR(r.sqr(), expected, 0.001);
+TEST(RbTest, Area1) {
+    Rb r(Tch(0,0), Tch(3,4), Tch(6,0), Tch(3,-4));
+    EXPECT_NEAR(r.get_s(), 24.0, 1e-6);
 }
 
-TEST(RmbTest, CenterCalculation) {
-    Tchk a(1,1), b(2,4), c(5,1), d(2,-2);
-    Rmb r(a,b,c,d);
-    
-    Tchk center = r.cntr();
-    EXPECT_NEAR(center.dx, 3.0, 0.001);
-    EXPECT_NEAR(center.dy, 1.0, 0.001);
+TEST(RbTest, Area2) {
+    Rb r(Tch(1,1), Tch(2,4), Tch(5,1), Tch(2,-2));
+    EXPECT_NEAR(r.get_s(), 12.0, 1e-6);
 }
 
-TEST(RmbTest, CopyAndCompare) {
-    Tchk a(0,0), b(2,3), c(4,0), d(2,-3);
-    Rmb r1(a,b,c,d);
-    Rmb* r2 = dynamic_cast<Rmb*>(r1.cpy());
-    
-    EXPECT_TRUE(r1.same(r2));
+TEST(RbTest, Center1) {
+    Rb r(Tch(0,0), Tch(3,4), Tch(6,0), Tch(3,-4));
+    Tch c = r.get_c();
+    EXPECT_NEAR(c.x_, 3.0, 1e-6);
+    EXPECT_NEAR(c.y_, 0.0, 1e-6);
+}
+
+TEST(RbTest, Center2) {
+    Rb r(Tch(1,2), Tch(3,6), Tch(7,2), Tch(3,-2));
+    Tch c = r.get_c();
+    EXPECT_NEAR(c.x_, 4.0, 1e-6);
+    EXPECT_NEAR(c.y_, 2.0, 1e-6);
+}
+
+TEST(RbTest, Copy) {
+    Rb r1(Tch(0,0), Tch(1,1), Tch(2,0), Tch(1,-1));
+    Fg* r2 = r1.make_copy();
+    EXPECT_TRUE(r1.is_same(r2));
     delete r2;
 }
 
-TEST(RmbTest, DifferentRhombus) {
-    Tchk a1(0,0), b1(2,3), c1(4,0), d1(2,-3);
-    Tchk a2(0,0), b2(3,4), c2(6,0), d2(3,-4);
-    Rmb r1(a1,b1,c1,d1), r2(a2,b2,c2,d2);
-    
-    EXPECT_FALSE(r1.same(&r2));
+TEST(RbTest, NotSame) {
+    Rb r1(Tch(0,0), Tch(1,1), Tch(2,0), Tch(1,-1));
+    Rb r2(Tch(0,0), Tch(2,2), Tch(4,0), Tch(2,-2));
+    EXPECT_FALSE(r1.is_same(&r2));
 }
 
-TEST(PngTest, AreaPositive) {
-    std::vector<Tchk> v = {
-        Tchk(0,0), Tchk(4,0), Tchk(5,2), 
-        Tchk(3,4), Tchk(0,3)
+TEST(PtTest, Area) {
+    std::vector<Tch> pts = {
+        Tch(0,0), Tch(4,0), Tch(5,2), 
+        Tch(3,4), Tch(0,3)
     };
-    Png p(v);
-    
-    EXPECT_GT(p.sqr(), 0);
+    Pt p(pts);
+    double area = p.get_s();
+    EXPECT_GT(area, 0.0);
+    EXPECT_LT(area, 20.0);
 }
 
-TEST(PngTest, CenterLocation) {
-    std::vector<Tchk> v = {
-        Tchk(0,0), Tchk(4,0), Tchk(4,4), 
-        Tchk(2,6), Tchk(0,4)
+TEST(PtTest, Center) {
+    std::vector<Tch> pts = {
+        Tch(0,0), Tch(4,0), Tch(4,4), 
+        Tch(2,6), Tch(0,4)
     };
-    Png p(v);
-    
-    Tchk center = p.cntr();
-    EXPECT_NEAR(center.dx, 2.0, 0.001);
-    EXPECT_NEAR(center.dy, 2.8, 0.001);
+    Pt p(pts);
+    Tch c = p.get_c();
+    EXPECT_NEAR(c.x_, 2.0, 1e-6);
+    EXPECT_NEAR(c.y_, 2.8, 1e-6);
 }
 
-TEST(PngTest, SelfComparison) {
-    std::vector<Tchk> v = {
-        Tchk(1,1), Tchk(3,1), Tchk(4,3), 
-        Tchk(2,5), Tchk(0,3)
+TEST(PtTest, Same) {
+    std::vector<Tch> pts = {
+        Tch(1,1), Tch(3,1), Tch(4,3), 
+        Tch(2,5), Tch(0,3)
     };
-    Png p1(v);
-    Png p2(v);
-    
-    EXPECT_TRUE(p1.same(&p2));
+    Pt p1(pts);
+    Pt p2(pts);
+    EXPECT_TRUE(p1.is_same(&p2));
 }
 
-TEST(ShgTest, RegularHexagonArea) {
-    std::vector<Tchk> v = {
-        Tchk(0,0), Tchk(2,0), Tchk(3,1), 
-        Tchk(2,2), Tchk(0,2), Tchk(-1,1)
+TEST(HxTest, Area) {
+    std::vector<Tch> pts = {
+        Tch(0,0), Tch(2,0), Tch(3,1), 
+        Tch(2,2), Tch(0,2), Tch(-1,1)
     };
-    Shg s(v);
-    
-    double area = s.sqr();
-    EXPECT_GT(area, 0);
-    EXPECT_LT(area, 10);
+    Hx h(pts);
+    double area = h.get_s();
+    EXPECT_NEAR(area, 6.0, 0.5);
 }
 
-TEST(ShgTest, CenterSymmetry) {
-    std::vector<Tchk> v = {
-        Tchk(-1,0), Tchk(0,1), Tchk(1,1),
-        Tchk(2,0), Tchk(1,-1), Tchk(0,-1)
+TEST(HxTest, Center) {
+    std::vector<Tch> pts = {
+        Tch(-1,0), Tch(0,1), Tch(1,1),
+        Tch(2,0), Tch(1,-1), Tch(0,-1)
     };
-    Shg s(v);
-    
-    Tchk center = s.cntr();
-    EXPECT_NEAR(center.dx, 0.5, 0.001);
-    EXPECT_NEAR(center.dy, 0.0, 0.001);
+    Hx h(pts);
+    Tch c = h.get_c();
+    EXPECT_NEAR(c.x_, 0.5, 1e-6);
+    EXPECT_NEAR(c.y_, 0.0, 1e-6);
 }
 
-TEST(ShgTest, DifferentHexagons) {
-    std::vector<Tchk> v1 = {
-        Tchk(0,0), Tchk(1,0), Tchk(1,1),
-        Tchk(0,1), Tchk(-1,1), Tchk(-1,0)
+TEST(HxTest, Different) {
+    std::vector<Tch> pts1 = {
+        Tch(0,0), Tch(1,0), Tch(1,1),
+        Tch(0,1), Tch(-1,1), Tch(-1,0)
     };
-    std::vector<Tchk> v2 = {
-        Tchk(0,0), Tchk(2,0), Tchk(3,1),
-        Tchk(2,2), Tchk(0,2), Tchk(-1,1)
+    std::vector<Tch> pts2 = {
+        Tch(0,0), Tch(2,0), Tch(3,1),
+        Tch(2,2), Tch(0,2), Tch(-1,1)
     };
-    Shg s1(v1), s2(v2);
-    
-    EXPECT_FALSE(s1.same(&s2));
+    Hx h1(pts1), h2(pts2);
+    EXPECT_FALSE(h1.is_same(&h2));
 }
 
-TEST(FArrTest, EmptyCollection) {
-    FArr fa;
-    EXPECT_EQ(fa.sz(), 0);
-    EXPECT_DOUBLE_EQ(fa.total(), 0.0);
+TEST(ColTest, Empty) {
+    Col c;
+    EXPECT_EQ(c.count(), 0);
+    EXPECT_DOUBLE_EQ(c.total_s(), 0.0);
 }
 
-TEST(FArrTest, AddMultipleFigures) {
-    FArr fa;
-    
-    Tchk a(0,0), b(1,1), c(2,0), d(1,-1);
-    fa.add(new Rmb(a,b,c,d));
-    
-    std::vector<Tchk> v = {
-        Tchk(0,0), Tchk(2,0), Tchk(3,1), 
-        Tchk(2,2), Tchk(0,2)
-    };
-    fa.add(new Png(v));
-    
-    EXPECT_EQ(fa.sz(), 2);
-    EXPECT_GT(fa.total(), 0);
+TEST(ColTest, AddOne) {
+    Col c;
+    c.add(new Rb(Tch(0,0), Tch(1,1), Tch(2,0), Tch(1,-1)));
+    EXPECT_EQ(c.count(), 1);
+    EXPECT_GT(c.total_s(), 0.0);
 }
 
-TEST(FArrTest, AddRemoveOperations) {
-    FArr fa;
+TEST(ColTest, AddMultiple) {
+    Col c;
+    c.add(new Rb(Tch(0,0), Tch(1,1), Tch(2,0), Tch(1,-1)));
     
-    Tchk a(0,0), b(1,1), c(2,0), d(1,-1);
-    fa.add(new Rmb(a,b,c,d));
-    
-    std::vector<Tchk> v = {
-        Tchk(0,0), Tchk(2,0), Tchk(3,1), 
-        Tchk(2,2), Tchk(0,2)
+    std::vector<Tch> pts = {
+        Tch(0,0), Tch(2,0), Tch(3,1), 
+        Tch(2,2), Tch(0,2)
     };
-    fa.add(new Png(v));
+    c.add(new Pt(pts));
     
-    EXPECT_EQ(fa.sz(), 2);
-    fa.del(0);
-    EXPECT_EQ(fa.sz(), 1);
-    fa.del(0);
-    EXPECT_EQ(fa.sz(), 0);
+    EXPECT_EQ(c.count(), 2);
 }
 
-TEST(FArrTest, InvalidIndexHandling) {
-    FArr fa;
+TEST(ColTest, Remove) {
+    Col c;
+    c.add(new Rb(Tch(0,0), Tch(1,1), Tch(2,0), Tch(1,-1)));
+    c.add(new Pt(std::vector<Tch>(5)));
     
-    Tchk a(0,0), b(1,1), c(2,0), d(1,-1);
-    fa.add(new Rmb(a,b,c,d));
-    
-    fa.del(5);
-    fa.del(-1);
-    
-    EXPECT_EQ(fa.sz(), 1);
+    EXPECT_EQ(c.count(), 2);
+    c.remove(0);
+    EXPECT_EQ(c.count(), 1);
+    c.remove(0);
+    EXPECT_EQ(c.count(), 0);
 }
 
-TEST(FArrTest, MixedFiguresArea) {
-    FArr fa;
+TEST(ColTest, RemoveInvalid) {
+    Col c;
+    c.add(new Rb(Tch(0,0), Tch(1,1), Tch(2,0), Tch(1,-1)));
     
-    Tchk r1(0,0), r2(2,3), r3(4,0), r4(2,-3);
-    fa.add(new Rmb(r1,r2,r3,r4));
+    c.remove(-1);
+    c.remove(5);
     
-    std::vector<Tchk> pv = {
-        Tchk(0,0), Tchk(3,0), Tchk(4,2), 
-        Tchk(2,4), Tchk(0,3)
-    };
-    fa.add(new Png(pv));
-    
-    std::vector<Tchk> hv = {
-        Tchk(0,0), Tchk(2,0), Tchk(3,1),
-        Tchk(2,2), Tchk(0,2), Tchk(-1,1)
-    };
-    fa.add(new Shg(hv));
-    
-    double total = fa.total();
-    EXPECT_GT(total, 0);
-    EXPECT_LT(total, 50);
+    EXPECT_EQ(c.count(), 1);
 }
 
-TEST(IntegrationTest, ComplexOperations) {
-    FArr fa;
+TEST(ColTest, TotalArea) {
+    Col c;
+    c.add(new Rb(Tch(0,0), Tch(3,4), Tch(6,0), Tch(3,-4))); // 24
+    c.add(new Pt(std::vector<Tch>{
+        Tch(0,0), Tch(2,0), Tch(3,1), 
+        Tch(2,2), Tch(0,2)
+    })); // ~3.5
     
-    Tchk a1(0,0), b1(1,2), c1(2,0), d1(1,-2);
-    fa.add(new Rmb(a1,b1,c1,d1));
+    double total = c.total_s();
+    EXPECT_GT(total, 27.0);
+    EXPECT_LT(total, 28.0);
+}
+
+TEST(ColTest, MixedTypes) {
+    Col c;
     
-    std::vector<Tchk> v1 = {
-        Tchk(1,1), Tchk(3,1), Tchk(4,3), 
-        Tchk(2,5), Tchk(0,3)
+    c.add(new Rb(Tch(0,0), Tch(2,3), Tch(4,0), Tch(2,-3)));
+    
+    std::vector<Tch> pent = {
+        Tch(0,0), Tch(3,0), Tch(4,2), 
+        Tch(2,4), Tch(0,3)
     };
-    fa.add(new Png(v1));
+    c.add(new Pt(pent));
     
-    double area1 = fa.total();
-    
-    std::vector<Tchk> v2 = {
-        Tchk(5,5), Tchk(7,5), Tchk(8,6),
-        Tchk(7,7), Tchk(5,7), Tchk(4,6)
+    std::vector<Tch> hex = {
+        Tch(0,0), Tch(2,0), Tch(3,1),
+        Tch(2,2), Tch(0,2), Tch(-1,1)
     };
-    fa.add(new Shg(v2));
+    c.add(new Hx(hex));
     
-    double area2 = fa.total();
+    EXPECT_EQ(c.count(), 3);
+    EXPECT_GT(c.total_s(), 0.0);
+}
+
+TEST(Integration, Complex) {
+    Col c;
+    
+    double area1 = c.total_s();
+    
+    c.add(new Rb(Tch(0,0), Tch(1,2), Tch(2,0), Tch(1,-2)));
+    double area2 = c.total_s();
     EXPECT_GT(area2, area1);
     
-    fa.del(1);
-    double area3 = fa.total();
-    EXPECT_LT(area3, area2);
+    std::vector<Tch> pent = {
+        Tch(1,1), Tch(3,1), Tch(4,3), 
+        Tch(2,5), Tch(0,3)
+    };
+    c.add(new Pt(pent));
+    double area3 = c.total_s();
+    EXPECT_GT(area3, area2);
+    
+    c.remove(0);
+    double area4 = c.total_s();
+    EXPECT_LT(area4, area3);
 }
 
 int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
